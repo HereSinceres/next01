2b37eddbcaaf422989b7f136062e94d726586887
徐凯 <sincerehere@outlook.com>
2023-11-27 01:47:26 +0800

    style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import "../styles/globals.css";
 import "./globals.css";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
